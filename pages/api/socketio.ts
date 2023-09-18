de2b40bc8e457b81b09a28180ad75f54250d4ed3
jeongminsang <zjzj@hanmail.net>
2023-09-18 21:13:04 +0900

    채팅 기능 구현

---
+++ pages/api/socketio.ts
@@ -0,0 +1,53 @@
+// import { Server } from "socket.io";
+
+// const socketioHandler = (req: any, res: any) => {
+//   if (!res.socket.server.io) {
+//     const io = new Server(res.socket.server);
+
+//     io.on("connection", (socket) => {
+//       socket.broadcast.emit("연결되었습니다.");
+//       socket.on("hello", (msg) => {
+//         socket.emit("hello", "world!");
+//       });
+//       console.log("연결됨");
+//       socket.on("roomsatu", function (data) {
+//         console.log(data);
+//         io.emit("dewanipara", data);
+//       });
+//     });
+
+//     res.socket.server.io = io;
+//   } else {
+//     console.log("socket.io already running");
+//   }
+//   res.end();
+// };
+
+// export default socketioHandler;
+
+import { Server } from "socket.io";
+
+const socketioHandler = (req: any, res: any) => {
+  if (!res.socket.server.io) {
+    const io = new Server(res.socket.server);
+
+    io.on("connection", (socket) => {
+      console.log("User connected succesfully");
+
+      socket.on("hello", (arg, callback) => {
+        callback(arg);
+      });
+
+      socket.on("sendmsg", (name, msg) => {
+        io.emit("receivemsg", name, msg);
+      });
+    });
+
+    res.socket.server.io = io;
+  } else {
+    console.log("socket.io already running");
+  }
+  res.end();
+};
+
+export default socketioHandler;
